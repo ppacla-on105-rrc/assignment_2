981c5d04b5b84333957129065e59ffca2af8ef01
Philip Pacla-on <ppacla-on105@academic.rrc.ca>
2026-02-08 18:25:30 -0600

    Updated app.ts to use the new route made.

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,12 @@
 import express, { Express } from "express";
 import morgan from "morgan";
+import healthRoutes from "./api/v1/routes/healthRoutes";
 
 const app: Express = express();
 
 app.use(express.json());
 app.use(morgan("combined"));
 
-app.get("/api/v1/health", (req, res) => {
-  res.status(200).json({
-    status: "OK",
-    uptime: process.uptime(),
-    timestamp: new Date().toISOString(),
-    version: "1.0.0",
-  });
-});
+app.use("/api/v1", healthRoutes);
 
 export default app;
